48f49141ccc5b78a7c71dfbb3e69942ecc15599a
u748 <carlos.vasquez@gmlaw.com>
2019-10-28 14:37:43 -0400

    more changes

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ var app = new Vue({
     el: "#app",
     data: {
         name: "Carlos Vasquez",
-        message: "Hello Vue"
+        message: "Hello Vue",
+        age: 25,
+        x: 0,
+        y: 0
+    },
+    methods: {
+        add: function (inc) {
+            //this.age++;
+            this.age += inc;
+        },
+        substract: function (dec) {
+            //this.age--;
+            this.age -= dec;
+        },
+        updateXY: function (event) {
+            console.log(event);
+            this.x = event.offsetX;
+            this.y = event.offsetY;
+        }
     }
 })
